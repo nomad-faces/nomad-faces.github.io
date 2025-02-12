d509c69d0e8a02bc705927d2d7b6ee6850a3617e
Sunny <sun616448@gmail.com>
2025-02-12 14:50:34 -0800

    add cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Scrollytelling from "./scrollytelling.js";
+import Cards from "./cards.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Scrollytelling />
+      <Cards />
     </div>
   )
 }

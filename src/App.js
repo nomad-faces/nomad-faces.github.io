cd1588cbddb454da6a0c24e1db7e4208afbe5902
Eden <edenhadar@gmail.com>
2025-03-12 15:38:20 -0700

    assets

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Scrollytelling from "./scrollytelling.js";
 import Cards from "./cards.js";
 import "./App.css";
 import Story from "./Story.js";
-import Story2 from "./Story2.js"; 
+import Story2 from "./Story2.js";
 import BubbleChart from "./charts/BubbleChart.jsx";
 import TopDestinations from "./charts/TopDestinations.jsx";
 import Age from "./charts/Age.jsx";
@@ -14,31 +14,7 @@ function App() {
     <div className="App">
       <Scrollytelling />
       <Cards />
-      <h1>Bubble Chart Visualization</h1>
-      <button onClick={() => setDataType("countries")}>Show Countries</button>
-      <button onClick={() => setDataType("ethnicities")}>
-        Show Ethnicities
-      </button>
-      <button onClick={() => setDataType("gender")}>Show Gender</button>
-      <button onClick={() => setDataType("education")}>Show Education</button>
 
-      <div
-        style={{
-          backgroundColor: "white", // Set background color to white
-          borderRadius: "10px", // Optional: Add rounded corners
-          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0)", // Optional: Add a subtle shadow
-          margin: "20px auto", // Center the container on the page with some margin
-          width: "80%", // Optional: Set the container width (adjust as needed)
-          maxWidth: "900px", // Optional: Set a max width
-        }}
-      >
-        <BubbleChart dataType={dataType} />
-      </div>
-      <Story />
-      <div>
-        <h1>Top Cities for Digital Nomads</h1>
-        <TopDestinations />
-        <Story2 />
       <div className="bubble-chart-section">
         <h1 className="bubble-chart-title">Digital Nomad Demographics</h1>
 
@@ -79,6 +55,7 @@ function App() {
           <BubbleChart dataType={dataType} />
         </div>
       </div>
+
       <h1 className="bubble-chart-title">Digital Nomad Age Distribution</h1>
       <div>
         <Age />
@@ -89,6 +66,7 @@ function App() {
         <h1>Top Destinations for Digital Nomads</h1>
         <TopDestinations />
       </div>
+      <Story2 />
     </div>
   );
 }
